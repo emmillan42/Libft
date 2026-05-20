03716f4fc33dffecaab5b47a5f48ac17599387b0
emmmilla <emmmilla@student.42madrid.com>
2026-05-20 15:29:14 +0200

    Update ft_is_alpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,13 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   isalpha.c                                          :+:      :+:    :+:   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emmmilla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 18:19:24 by emmmilla          #+#    #+#             */
-/*   Updated: 2026/05/19 18:19:37 by emmmilla         ###   ########.fr       */
+/*   Updated: 2026/05/20 14:43:56 by emmmilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+//Function that checks  for an alphabetic character
+int	ft_isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
+
+/*//Testing the function with some strings
+#include <stdio.h>
+#include <ctype.h>
+
+int	main(void)
+{
+	printf("char\tprint\tisalpha\tft_isalpha\n");
+	for (int i = 0; i <= 255; i++)
+	{
+		printf("%d\t", i);
+		if (ft_isprint(i))
+			printf("%c\t", i);
+		else
+			printf("-\t");
+		printf("%d\t", isalpha(i));
+		printf("%d\n", ft_isalpha(i));
+	}
+	return (0);
+}*/
