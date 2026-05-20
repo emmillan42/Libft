f050d5ac58cef420f98addbf5f961c8189bd8925
emmmilla <emmmilla@student.42madrid.com>
2026-05-20 15:30:02 +0200

    Update ft_is_ascii.c

--- ft_isascii.c
+++ ft_isascii.c
@@ -1,13 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   isascii.c                                          :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emmmilla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 18:21:51 by emmmilla          #+#    #+#             */
-/*   Updated: 2026/05/19 18:21:58 by emmmilla         ###   ########.fr       */
+/*   Updated: 2026/05/20 15:22:07 by emmmilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+//Function that checks whether c is a 7-bit unsigned char value that fits into
+//the ASCII character set
+int	ft_isascii(int c)
+{
+	if (c >= 0 && c<= 127)
+		return (1);
+	else
+		return (0);
+}
+
+/*//Testing the function with some characters
+#include <stdio.h>
+#include <ctype.h>
+
+int	main(void)
+{
+	printf("char\tprint\tisascii\tft_isascii\n");
+	for (int i = 0; i <= 255; i++)
+	{
+		printf("%d\t", i);
+		if (ft_isprint(i))
+			printf("%c\t", i);
+		else
+			printf("-\t");
+		printf("%d\t", isascii(i));
+		printf("%d\n", ft_isascii(i));
+	}
+	return (0);
+}*/
