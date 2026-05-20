0a51a3bc4f49d80a36fe3250b5e46a209bf5603d
emmmilla <emmmilla@student.42madrid.com>
2026-05-20 15:29:50 +0200

    Update ft_is_alnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,13 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   isalnum.c                                          :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emmmilla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 18:21:12 by emmmilla          #+#    #+#             */
-/*   Updated: 2026/05/19 18:21:21 by emmmilla         ###   ########.fr       */
+/*   Updated: 2026/05/20 15:21:38 by emmmilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+//Function that checks for an alphanumeric character; 
+//it is equivalent to (isalpha(c) || isdigit(c))
+int	ft_isalnum(int c)
+{
+	if ((c >= '0' && c <= '9')
+		|| (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
+
+/*//Testing the function with some characters
+#include <stdio.h>
+#include <ctype.h>
+
+int	main(void)
+{
+	printf("char\tprint\tisalnum\tft_isalnum\n");
+	for (int i = 0; i <= 255; i++)
+	{
+		printf("%d\t", i);
+		if (ft_isprint(i))
+			printf("%c\t", i);
+		else
+			printf("-\t");
+		printf("%d\t", isalnum(i));
+		printf("%d\n", ft_isalnum(i));
+	}
+	return (0);
+}*/
