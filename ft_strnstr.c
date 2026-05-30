b1a1bf5249965640d9f5070c97fd5259ff87c6fd
emmmilla <emmmilla@student.42madrid.com>
2026-05-30 22:15:16 +0200

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,4 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+//Function that locates the first occurrence of the null-terminated string
+//little in the string big, where not more than len characters are searched.
+//Characters that appear after a `\0' character are not searched.
+char	*strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (*little == '\0')
+		return ((char *)big);
+	while (i < len && big[i])
+	{
+		j = 0;
+		if (big[i] == little[0])
+		{
+			while (i + j < len && big[i + j] == little[j])
+			{
+				j++;
+				if (little[j] == '\0')
+					return ((char *)&big[i]);
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
