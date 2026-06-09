c467cd46b0cc3852508382f24ea890ecb758a0d3
emmmilla <emmilla@student.42madrid.com>
2026-06-09 22:52:50 +0200

    Update ft_striteri.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -10,4 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	i;
+
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
+
+/* //Testing the Function with some strings
+#include <stdio.h>
+
+void	roller_coaster(unsigned int i, char *c)
+{
+	if (i % 2)
+		*c = ft_tolower(*c);
+	else
+		*c = ft_toupper(*c);
+}
+
+int	main(void)
+{
+	char	str[] = "Hello everyone. Good to see you again !";
+
+	printf("Before ft_striteri : %s\n", str);
+	ft_striteri(str, &roller_coaster);
+	printf("After ft_striteri : %s\n", str);
+	return (0);
+} */
