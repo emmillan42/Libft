9e9dcf6171df25c01868237695b91f794ca5236f
emmmilla <emmmilla@student.42madrid.com>
2026-06-10 11:13:59 +0200

    Update ft_putendl_fd.c

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,8 +6,44 @@
 /*   By: emmmilla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 19:00:05 by emmmilla          #+#    #+#             */
-/*   Updated: 2026/05/19 19:00:07 by emmmilla         ###   ########.fr       */
+/*   Updated: 2026/06/10 11:13:05 by emmmilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_putendl_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
+
+/* //Testing the function with some strings
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*test_cases[] = {"Hello 42!", "Coucou 42!", "Hola 42!"};
+	int		fd = open("ft_putendl_fd_test", O_CREAT | O_RDWR, 0777);
+
+	printf("Standard input :\n");
+	for (size_t i = 0; i < sizeof(test_cases) / sizeof(test_cases[0]); i++)
+		ft_putendl_fd(test_cases[i], STDIN_FILENO);
+	printf("Standard output :\n");
+	for (size_t i = 0; i < sizeof(test_cases) / sizeof(test_cases[0]); i++)
+		ft_putendl_fd(test_cases[i], STDOUT_FILENO);
+	printf("Standard error :\n");
+	for (size_t i = 0; i < sizeof(test_cases) / sizeof(test_cases[0]); i++)
+		ft_putendl_fd(test_cases[i], STDERR_FILENO);
+	if (fd != -1)
+	{
+		printf("New file : check file ft_putendl_fd_test\n");
+		for (size_t i = 0; i < sizeof(test_cases) / sizeof(test_cases[0]); i++)
+			ft_putendl_fd(test_cases[i], fd);
+		close(fd);
+	}
+	return (0);
+} */
