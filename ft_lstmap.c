469c9e62cd8816eb7e1a355da1c9a0ae80031678
emmmilla <emmilla@student.42madrid.com>
2026-06-11 17:46:14 +0200

    Update ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,11 +3,86 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emmmilla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: emmmilla <emmmilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 19:08:26 by emmmilla          #+#    #+#             */
-/*   Updated: 2026/05/19 19:08:28 by emmmilla         ###   ########.fr       */
+/*   Updated: 2026/06/11 17:45:08 by emmmilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_lst;
+	t_list	*new_node;
+	void	*content;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	new_lst = NULL;
+	while (lst)
+	{
+		content = f(lst->content);
+		new_node = ft_lstnew(content);
+		if (!new_node)
+		{
+			del(content);
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, new_node);
+		lst = lst->next;
+	}
+	return (new_lst);
+}
+
+/* //Testing the Function with some values
+#include <stdio.h>
+
+void	ft_print_content(void *content)
+{
+	printf("\"%s\" -> ", (char *)content);
+}
+
+void	ft_print_list(t_list *lst)
+{
+	printf("Printing list : ");
+	ft_lstiter(lst, &ft_print_content);
+	printf("NULL\n");
+}
+
+void	ft_del_content(void *content)
+{
+	printf("deleting \"%s\"\n", (char *)content);
+}
+
+void	*ft_same(void *content)
+{
+	return (content);
+}
+
+int	main(void)
+{
+	t_list	*lst;
+	char	*contents[] = {"Hello", "everyone", "!!!"};
+
+	lst = NULL;
+	ft_print_list(lst);
+	for (size_t i = 0; i < sizeof(contents) / sizeof(contents[0]); i++)
+	{
+		t_list	*node = ft_lstnew(contents[i]);
+		if (!node)
+			printf("Memory allocation failed.\n");
+		else
+			ft_lstadd_back(&lst, node);
+	}
+	ft_print_list(lst);
+	t_list	*lst_cap = ft_lstmap(lst, &ft_same, &ft_del_content);
+	ft_print_list(lst_cap);
+	ft_lstclear(&lst, &ft_del_content);
+	ft_lstclear(&lst_cap, &ft_del_content);
+	ft_print_list(lst);
+	ft_print_list(lst_cap);
+	return (0);
+} */
