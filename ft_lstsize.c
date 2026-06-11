67ae5c9cf3da3fe8e92f6b7eb8fd95fc86aa5084
emmmilla <emmilla@student.42madrid.com>
2026-06-11 08:22:04 +0200

    Update ft_lstsize.c

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -3,11 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstsize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emmmilla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: emmmilla <emmmilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 19:05:33 by emmmilla          #+#    #+#             */
-/*   Updated: 2026/05/19 19:05:36 by emmmilla         ###   ########.fr       */
+/*   Updated: 2026/06/11 08:20:34 by emmmilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	ft_lstsize(t_list *lst)
+{
+	int	size;
+
+	size = 0;
+	while (lst)
+	{
+		size++;
+		lst = lst->next;
+	}
+	return (size);
+}
+
+/* // Testing the function with some values
+#include <stdio.h>
+
+void	ft_print_content(void *content)
+{
+	printf("\"%s\" -> ", (char *)content);
+}
+
+void	ft_print_list(t_list *lst)
+{
+	printf("Printing list : ");
+	ft_lstiter(lst, &ft_print_content);
+	printf("NULL\n");
+}
+
+void	ft_del_content(void *content)
+{
+	printf("deleting \"%s\"\n", (char *)content);
+}
+
+int	main(void)
+{
+	t_list	*lst;
+	char	*contents[] = {"Hello", "les", "cocos"};
+
+	lst = NULL;
+	ft_print_list(lst);
+	printf("Size of list : %d\n", ft_lstsize(lst));
+	for (size_t i = 0; i < sizeof(contents) / sizeof(contents[0]); i++)
+	{
+		t_list	*node = ft_lstnew(contents[i]);
+		if (!node)
+			printf("Memory allocation failed.\n");
+		else
+			ft_lstadd_back(&lst, node);
+	}
+	ft_print_list(lst);
+	printf("Size of list : %d\n", ft_lstsize(lst));
+	ft_lstclear(&lst, &ft_del_content);
+	ft_print_list(lst);
+	printf("Size of list : %d\n", ft_lstsize(lst));
+	return (0);
+} */
