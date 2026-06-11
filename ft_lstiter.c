7b3509ae71e73575272e14f6ac1835674b85016a
emmmilla <emmmilla@student.42madrid.com>
2026-06-11 14:15:07 +0200

    Update ft_lstiter.c

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -3,11 +3,72 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstiter.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emmmilla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: emmmilla <emmmilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 19:08:04 by emmmilla          #+#    #+#             */
-/*   Updated: 2026/05/19 19:08:06 by emmmilla         ###   ########.fr       */
+/*   Updated: 2026/06/11 14:14:31 by emmmilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!f)
+		return ;
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
+
+/* //RECURSIVE VERSION
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!lst || !f)
+		return ;
+	f(lst->content);
+	ft_lstiter(lst->next, f);
+}
+*/
+
+/* //Testing the function with some values
+#include <stdio.h>
+
+void	ft_print_content(void *content)
+{
+	printf("\"%s\" -> ", (char *)content);
+}
+
+void	ft_print_list(t_list *lst)
+{
+	printf("Printing list : ");
+	ft_lstiter(lst, &ft_print_content);
+	printf("NULL\n");
+}
+
+void	ft_del_content(void *content)
+{
+	printf("deleting \"%s\"\n", (char *)content);
+}
+
+int	main(void)
+{
+	t_list	*lst;
+	char	*contents[] = {"Hello", "les", "cocos"};
+
+	lst = NULL;
+	ft_print_list(lst);
+	for (size_t i = 0; i < sizeof(contents) / sizeof(contents[0]); i++)
+	{
+		t_list	*node = ft_lstnew(contents[i]);
+		if (!node)
+			printf("Memory allocation failed.\n");
+		else
+			ft_lstadd_back(&lst, node);
+	}
+	ft_print_list(lst);
+	ft_lstclear(&lst, &ft_del_content);
+	return (0);
+} */
